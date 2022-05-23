3342870248d5fe38f89710370b823a186e1e7771
si-shakil <sishakil850@gmail.com>
2022-05-23 15:26:22 +0600

    db connected

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ app.use(express.json());
 
 //connecting database
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@redonion.uipb9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    client.connect();
+    const productCollection = client
+      .db("tools_manufacturer")
+      .collection("products");
+    const orderCollection = client
+      .db("tools_manufacturer")
+      .collection("orders");
+  } finally {
+    //client.close()
+  }
+}
+run();
 app.get("/", (req, res) => {
   res.send("Tools Manufacturer Server Side is Running");
 });
